13093ccc45866a3f83eec70e9c5cfdf9549bd84d
SoLoGHoST <solomon_closson@yahoo.com>
2018-04-12 10:16:58 -0400

    background color change and moves to .load.

--- templates/tif-featherlight-slider/tif-featherlight-slider.js
+++ templates/tif-featherlight-slider/tif-featherlight-slider.js
@@ -2,12 +2,13 @@
 
 jQuery(document).ready(function($) {
 
-	$('#tif-featherlight-slider').featherlightGallery();
-	
-
 	$('.featherlight-link').click(function() {
 		$('#tif-featherlight-slider').find('.featherlight-first-slide').trigger('click');    
 	});
 
+	// Let the page load before calling the featherlightGallery plugin, makes images slide smoother!
+	$(window).load(function() {
+		$('#tif-featherlight-slider').featherlightGallery();
+	});
 
 });
